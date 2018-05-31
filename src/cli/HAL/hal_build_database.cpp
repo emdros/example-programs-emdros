cab431d1e1c5db0d3c3aa8989d116570e33b9764
Ulrik Sandborg-Petersen <ulrikp@scripturesys.com>
2018-05-31 18:45:14 +0200

    Fix license information: It's MIT, and it's in the LICENSE file.

--- src/cli/HAL/hal_build_database.cpp
+++ src/cli/HAL/hal_build_database.cpp
@@ -12,29 +12,8 @@
  *   Emdros - the database engine for analyzed or annotated text
  *   Copyright (C) 2003-2017  Ulrik Sandborg-Petersen
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, license version 2.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *
- *   Other licensing forms
- *   =====================
- *
- *   If you wish to negotiate commercial licensing, please contact
- *   Ulrik Petersen at ulrikp[at]users.sourceforge.net.
- *
- *   Licensing can also be negotiated if your organization is an
- *   educational, non-profit, charity, missionary or similar
- *   organization.
+ *   See the file LICENSE in the root of the sources for copyright
+ *   information.
  *
  **************************************************************************/
 
